2875b3266ebbd362e76fa82e48ca38cac6b475d0
Karine C <karine.cordeiro1998@hotmail.com>
2022-10-05 16:18:18 -0300

    mudança de imagem pessoal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#switch')
-const imagem = window.document.getElementById('imagem')
+let imagem = window.document.getElementById('imagem')
 
 $checkbox.addEventListener ('change', function(){
     $html.classList.toggle('dark-mode')
-    imagem.src = 'assets/imagem/fotoroxa.png'
+    // imagem.src = 'assets/imagem/fotoroxa.png' 
 
+    // if(imagem.checked){
+    //     imagem.src = 'assets/imagem/foto.png'
+    // }
+    
 })
