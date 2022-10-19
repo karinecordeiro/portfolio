0e643ac76bf3d6f8349e1acd4a0e6a9d2c4f1139
Karine C <karine.cordeiro1998@hotmail.com>
2022-10-19 14:01:28 -0300

    alteração de fonte de textos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,4 @@ let imagem = window.document.getElementById('imagem')
 
 $checkbox.addEventListener ('change', function(){
     $html.classList.toggle('dark-mode')
-    // imagem.src = 'assets/imagem/fotoroxa.png' 
-
-    // if(imagem.checked){
-    //     imagem.src = 'assets/imagem/foto.png'
-    // }
-    
 })
